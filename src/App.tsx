78ad0f10d252f3218cfb663fda9a9ae15f804da5
Ash Beech <ash@eatplants.cc>
2021-08-13 11:45:57 +0100

    .

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { gql } from "@apollo/client";
+import React from "react";
+import { Container } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/react";
+import { Chains, Subgraph, Subgraphs, TheGraphProvider, useCreateSubgraph, useSubgraph } from "thegraph-react";
+
+function Huddl({ huddl }: {
+  readonly huddl: Subgraph,
+}): JSX.Element {
+  const { useQuery } = useSubgraph(huddl);
+
+  const { error, loading, data } = useQuery(gql`
+  {
+    exampleEntities(first: 10) {
+      id
+      count
+      owner
+      approved
+    }
+  }
+  `);
+
+  //Debug in-browser
+  console.log(data);
+
+  return (
+    <Container>
+      <Text>{(error || loading) ? 'Loading...' : JSON.stringify(data)}</Text>
+    </Container>
+  );
+}
+
+
+export default function App(): JSX.Element {
+  const huddl = useCreateSubgraph({
+    [Chains.MAINNET]: 'https://api.studio.thegraph.com/query/5422/akl4m8c9/0.0.1',
+  });
+
+  const subgraphs = React.useMemo((): Subgraphs => {
+    return [huddl];
+  }, [huddl]);
+
+  return (
+    <TheGraphProvider chain={Chains.MAINNET} subgraphs={subgraphs}>
+      <Huddl huddl={huddl} />
+    </TheGraphProvider>
+  );
+}
