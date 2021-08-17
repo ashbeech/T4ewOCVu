c9c53c698f83cd78d41a7cbbd72017ee6299d09f
Ash Beech <ash@eatplants.cc>
2021-08-17 16:52:53 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,73 @@
+import React, { useState, useEffect } from "react";
 import { gql } from "@apollo/client";
-import React from "react";
 import { Container } from "@chakra-ui/layout";
-import { Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Chains, Subgraph, Subgraphs, TheGraphProvider, useCreateSubgraph, useSubgraph } from "thegraph-react";
 
-function Huddl({ huddl }: {
-  readonly huddl: Subgraph,
-}): JSX.Element {
-  const { useQuery } = useSubgraph(huddl);
+const version = "0.0.5";
+const subgraph = "akl4m8c9";
+const api_url = 'https://api.studio.thegraph.com/query/5422/' + subgraph + '/' + version; // <-- ENTER YOUR SUBGRAPH NODE URL HERE
+const input = Object();
 
+function Parse(input:unknown) {
+
+  let _input = Object(input);
+  //Debug in-browser
+  //console.log(_input);
+
+  let render = JSON.parse(_input.token.tokenURI);
+
+  return (
+    <img src={render} alt=""/>
+    );
+
+}
+
+function Huddl({ tokenData }: {
+  readonly tokenData: Subgraph,
+}): JSX.Element {
+  const { useQuery } = useSubgraph(tokenData);
+  
   // ENTER YOUR QUERY HERE
-  const { error, loading, data } = useQuery(gql`
+  // ID currently hardcoded for testing
+  const { error, loading, data} = useQuery(gql`
   {
-    exampleEntities(first: 10) {
+    token(id:"0xe9b6db0bb21e6df1fe6f8c5e076740eebf4d81eb_2836"){
       id
-      count
+      contract
+      tokenID
       owner
-      approved
+      mintTime
+      tokenURI
     }
   }
-  `);
 
-  //Debug in-browser
-  console.log(data);
+  `);
 
   return (
     <Container>
-      <Text>{(error || loading) ? 'Loading...' : JSON.stringify(data)}</Text>
+      <Box>
+        {(error || loading) ? "Loading…" :
+        Parse(data)
+        }
+      </Box>
     </Container>
   );
 }
 
-
 export default function App(): JSX.Element {
-  const huddl = useCreateSubgraph({
-    [Chains.MAINNET]: 'https://api.studio.thegraph.com/query/5422/akl4m8c9/0.0.1', // <-- ENTER YOUR QUERY URL HERE
+
+  const tokenData = useCreateSubgraph({
+    [Chains.MAINNET]: api_url, // <-- ENTER YOUR QUERY URL HERE
   });
 
   const subgraphs = React.useMemo((): Subgraphs => {
-    return [huddl];
-  }, [huddl]);
+    return [tokenData];
+  }, [tokenData]);
 
   return (
     <TheGraphProvider chain={Chains.MAINNET} subgraphs={subgraphs}>
-      <Huddl huddl={huddl} />
+      <Huddl tokenData={tokenData} />
     </TheGraphProvider>
   );
 }
